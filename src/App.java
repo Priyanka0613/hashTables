6e1f3e96ab24240ef65eba46f39d80feb7694c8e
Priyanka <priyankaag.0613@gmail.com>
2023-12-28 01:37:34 +0530

    uc2

--- src/App.java
+++ src/App.java
@@ -3,19 +3,15 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        String sentence = "To be or not to be";
+        String sentence = "Paranoids are not paranoid because they are paranoid but because they keep putting themselves deliberately into paranoid avoidable situations";
         String[] words = sentence.split("\\s+");
 
         Map<String, Integer> wordFrequencyMap = new HashMap<>();
 
         for (String word : words) {
             String lowercaseWord = word.toLowerCase();
-            wordFrequencyMap.put(lowercaseWord, wordFrequencyMap.getOrDefault(lowercaseWord, 0) + 1);
+            wordFrequencyMap.put(lowercaseWord, wordFrequencyMap.getOrDefault(lowercaseWord, 0) +1);
         }
-
-        for(String word :words) {
-            wordFrequencyMap.put(word,wordFrequencyMap.getOrDefault(word,1) +1) ;
-       }
 
         for (Map.Entry<String, Integer> entry : wordFrequencyMap.entrySet()) {
             System.out.println(entry.getKey() + ": " + entry.getValue());
