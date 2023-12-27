16556fa4e33d6540408b6aafa19f90d0e9a747b4
Priyanka <priyankaag.0613@gmail.com>
2023-12-28 01:31:37 +0530

    UC1

--- src/App.java
+++ src/App.java
@@ -3,6 +3,22 @@ import java.util.Map;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Frequency Map");
+        String sentence = "To be or not to be";
+        String[] words = sentence.split("\\s+");
+
+        Map<String, Integer> wordFrequencyMap = new HashMap<>();
+
+        for (String word : words) {
+            String lowercaseWord = word.toLowerCase();
+            wordFrequencyMap.put(lowercaseWord, wordFrequencyMap.getOrDefault(lowercaseWord, 0) + 1);
+        }
+
+        for(String word :words) {
+            wordFrequencyMap.put(word,wordFrequencyMap.getOrDefault(word,1) +1) ;
+       }
+
+        for (Map.Entry<String, Integer> entry : wordFrequencyMap.entrySet()) {
+            System.out.println(entry.getKey() + ": " + entry.getValue());
+        }
     }
 }
